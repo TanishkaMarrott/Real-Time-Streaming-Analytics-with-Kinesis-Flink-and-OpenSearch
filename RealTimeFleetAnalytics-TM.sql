2ac29e56592778ef272c93ffa9aa3f09a7d6cc1b
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-05-02 15:56:38 +0530

    Non-functional enhancements for lambda

--- RealTimeFleetAnalytics-TM.sql
+++ RealTimeFleetAnalytics-TM.sql
@@ -1,26 +1,24 @@
 -- Dropping existing tables if they exist
-
 %flink.ssql(type=update)
--- Remove the existing 'taxi_trips' table
 DROP TABLE IF EXISTS taxi_trips;
 
--- Create 'taxi_trips' table with data streamed from Kinesis
+-- Creating a new 'taxi_trips' table with data streamed from Kinesis
 CREATE TABLE taxi_trips (
     id STRING,
-    vendorId INTEGER, 
-    pickupDate STRING, 
-    dropoffDate STRING, 
-    passengerCount INTEGER, 
-    pickupLongitude DOUBLE, 
-    pickupLatitude DOUBLE, 
-    dropoffLongitude DOUBLE, 
-    dropoffLatitude DOUBLE, 
-    storeAndFwdFlag STRING, 
-    gcDistance DOUBLE, 
-    tripDuration INTEGER, 
+    vendorId INTEGER,
+    pickupDate STRING,
+    dropoffDate STRING,
+    passengerCount INTEGER,
+    pickupLongitude DOUBLE,
+    pickupLatitude DOUBLE,
+    dropoffLongitude DOUBLE,
+    dropoffLatitude DOUBLE,
+    storeAndFwdFlag STRING,
+    gcDistance DOUBLE,
+    tripDuration INTEGER,
     googleDistance INTEGER,
-    googleDuration INTEGER
-
+    googleDuration INTEGER,
+    source STRING
 ) WITH (
     'connector' = 'kinesis',
     'stream' = 'input-stream',
@@ -30,10 +28,9 @@ CREATE TABLE taxi_trips (
 );
 
 %flink.ssql(type=update)
--- Remove the existing 'trip_statistics' table
 DROP TABLE IF EXISTS trip_statistics;
 
--- Create 'trip_statistics' table in OpenSearch to store aggregated trip data
+-- Creating 'trip_statistics' table in OpenSearch to store aggregated trip data
 CREATE TABLE trip_statistics (
     trip_count BIGINT,
     passenger_count INTEGER,
@@ -41,7 +38,10 @@ CREATE TABLE trip_statistics (
     average_trip_duration DOUBLE,
     average_trip_distance DOUBLE,
     peak_hour INTEGER,
-    trips_during_peak INTEGER
+    trips_during_peak INTEGER,
+    masked_average_pickup_longitude DOUBLE,
+    masked_average_pickup_latitude DOUBLE,
+    anomaly_score DOUBLE
 ) WITH (
     'connector' = 'elasticsearch-7',
     'hosts' = 'https://search-os-domain-yourdomain.us-west-2.es.amazonaws.com:443',
@@ -50,33 +50,25 @@ CREATE TABLE trip_statistics (
     'password' = 'YourPassword'
 );
 
--- Aggregating and inserting data into 'trip_statistics'
-
+-- Inserting aggregated data into 'trip_statistics' with anomaly detection and data masking
 %flink.ssql(type=update)
--- Insert aggregated data from 'taxi_trips' into 'trip_statistics'
 INSERT INTO trip_statistics
 SELECT 
-    COUNT(1) AS trip_count, 
-    SUM(passengerCount) AS passenger_count, 
+    COUNT(1) AS trip_count,
+    SUM(passengerCount) AS passenger_count,
     SUM(tripDuration) AS total_trip_duration,
-    AVG(tripDuration) AS average_trip_duration, 
+    AVG(tripDuration) AS average_trip_duration,
     AVG(gcDistance) AS average_trip_distance,
-    (
-        SELECT HOUR(pickupDate) 
-        FROM taxi_trips 
-        GROUP BY HOUR(pickupDate)
-        ORDER BY COUNT(*) DESC
-        LIMIT 1
-    ) AS peak_hour,
-    (
-        SELECT COUNT(*) 
-        FROM taxi_trips 
-        WHERE HOUR(pickupDate) = peak_hour
-    ) AS trips_during_peak
+    ROUND(AVG(pickupLongitude) + RAND() * 0.01, 5) AS masked_average_pickup_longitude,
+    ROUND(AVG(pickupLatitude) + RAND() * 0.01, 5) AS masked_average_pickup_latitude,
+    CALCULATE_ANOMALY_SCORE(tripDuration, passengerCount) AS anomaly_score,
+    (SELECT HOUR(pickupDate) FROM taxi_trips GROUP BY HOUR(pickupDate) ORDER BY COUNT(*) DESC LIMIT 1) AS peak_hour,
+    (SELECT COUNT(*) FROM taxi_trips WHERE HOUR(pickupDate) = peak_hour) AS trips_during_peak
 FROM taxi_trips
 WHERE pickupLatitude <> 0 AND pickupLongitude <> 0 AND dropoffLatitude <> 0 AND dropoffLongitude <> 0;
 
--- Additional  Queries
+-- Define a user-defined function for anomaly detection (placeholder, needs actual UDF implementation)
+CREATE FUNCTION CALCULATE_ANOMALY_SCORE AS 'com.yourdomain.flinksqlexample.AnomalyScoreCalculator';
 
 
 %flink.ssql(type=update)
@@ -100,4 +92,3 @@ SELECT DAYOFWEEK(pickupDate) AS weekday, HOUR(pickupDate) AS hour, COUNT(*) AS t
 FROM taxi_trips
 GROUP BY DAYOFWEEK(pickupDate), HOUR(pickupDate)
 ORDER BY trip_count DESC;
-
